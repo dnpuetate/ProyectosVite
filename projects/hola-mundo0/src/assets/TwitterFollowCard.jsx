79e4c48ebed7159d7b1f2f08b8d1fb70a223ce50
dnpuetate <49051986+dnpuetate@users.noreply.github.com>
2023-08-21 20:02:18 -0500

    bottton - estado pt2

--- projects/hola-mundo0/src/assets/TwitterFollowCard.jsx
+++ projects/hola-mundo0/src/assets/TwitterFollowCard.jsx
@@ -1,32 +1,45 @@
+//hooks
+import { useState } from 'react'
 import './App.css'
-export function TwitterFollowCard ({children, userName = undefined, isFollowind}){
+export function TwitterFollowCard({ children, userName}) {
 
     //const addAtt = (userName) => `@${userName}`
-   // console.log(isFollowind)
+    // console.log(isFollowind)
+
+    const [isFollowind, setIsFollowind] = useState(false)
+    /*const state = useState(false)
+    const isFollowind = state[0]
+    const setIsFollowind = state[1]*/
+
     const text = isFollowind ? 'Siguiendo' : 'Seguir'
     const buttonClassName = isFollowind
         ? 'tw-followCard-button is-followind'
         : 'tw-followCard-button'
 
+    const handleClick = () => {
+        setIsFollowind(!isFollowind)
+    }
+        
+
     return (
         <article className='tw-followCard'>
-            <header 
+            <header
                 className='tw-followCard-header'>
                 <img className='tw-followCard-avatar'
-                src={`https://unavatar.io/${userName}`}
-                alt='El avatar de dayan' 
-            />
-            <div className='tw-followCard-info'>
-                <strong>{children}</strong>
-                <span className='tw-followCard-infouserName'>@{userName}</span>
-            </div>
+                    src={`https://unavatar.io/${userName}`}
+                    alt='El avatar de dayan'
+                />
+                <div className='tw-followCard-info'>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infouserName'>@{userName}</span>
+                </div>
             </header>
             <aside>
-                <button className={buttonClassName}>
+                <button className={buttonClassName} onClick={handleClick}>
                     {text}
                 </button>
             </aside>
         </article>
     )
-    
+
 }
