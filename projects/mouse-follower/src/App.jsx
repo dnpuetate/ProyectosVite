0f9c86f1c0caa03bc14a46d76b4b08f58c61af39
dnpuetate <49051986+dnpuetate@users.noreply.github.com>
2023-09-06 18:44:35 -0500

    poner un toggle mounted followMouse

--- projects/mouse-follower/src/App.jsx
+++ projects/mouse-follower/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 
-function App () {
+// eslint-disable-next-line no-unused-vars
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
@@ -13,32 +14,56 @@ function App () {
       const { clientX, clientY } = event
 
       console.log('handleMove', { clientX, clientY })
+
+      setPosition({ x: clientX, y: clientY })
     }
 
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
     }
+
+    // cleanup
+    // se ejecuta cuando:
+    // -> cuando el componente se desmonta, ej: una ventana
+    // -> cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
   }, [enabled]) // este efecto se tiene que ejecutar para vez que cambie el efecto de enable
 
   return (
-    <main>
+    <>
       <div
         style={{
           position: 'absolute',
-          backgroundColor: '#09f',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
           borderRadius: '50%',
           opacity: 0.8,
           pointerEvents: 'none',
-          left: -20,
-          top: -20,
-          width: 48,
-          height: 40,
-          transform: 'translate(0px, 0px)'
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
         }}
       />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
+    </>
+  )
+}
+
+function App () {
+  const [mounted, setMounted] = useState(true)
+
+  return (
+    <main>
+      {mounted && <FollowMouse/>}
+       <button onClick={() => setMounted(!mounted)}>
+        Toggle mounted FollowMouse component
+       </button>
     </main>
   )
 }
--- projects/tic-tac-toe/src/components/Square.jsx
+++ projects/tic-tac-toe/src/components/Square.jsx
@@ -1,22 +1,20 @@
 // FUNCION SQUARE ----------------------------------------------
 export const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
 
-    const className = `square ${isSelected ? 'is-selected' : ''}`
-  
-    /*PASO 3. LLAMAR A LA FUNCION UPDATEBORAD PARA ACUALIZAR EL TABLERO
+  /* PASO 3. LLAMAR A LA FUNCION UPDATEBORAD PARA ACUALIZAR EL TABLERO
       envia el "index" para saber la posicion del click en el tablero
     */
-    const handleClick = () => {
-      updateBoard(index)
-    }
-  
-    //PASO 2. CUANDO ALGUIEN HAGA CLICK EN LOS CUADRADOS, EJECUTA HANDLECLIC
-    return (
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  // PASO 2. CUANDO ALGUIEN HAGA CLICK EN LOS CUADRADOS, EJECUTA HANDLECLIC
+  return (
       <div onClick={handleClick} className={className}>
         {children}
       </div>
-    )
-  }
-  
-  
-  //-----------------------------------------------------------
+  )
+}
+
+// -----------------------------------------------------------
