3be33247b27909f689b8e6d79d758f20eb39a68f
dnpuetate <49051986+dnpuetate@users.noreply.github.com>
2023-09-10 21:23:38 -0500

    PT-PT2
    FETCH A 2 APIS

--- projects/prueba-tecnica2/src/App.jsx
+++ projects/prueba-tecnica2/src/App.jsx
@@ -1,21 +1,41 @@
 import { useEffect, useState } from 'react'
-
+import './App.css'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
-
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 export function App () {
-  const [fact, setFact] = useState('lorem input cat fact whatever')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const threefirstWords = fact.split(' ', 3).join(' ')
+        console.log(threefirstWords)
+        fetch(`https://cataas.com/cat/says/${threefirstWords}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageUrl(url)
+            // console.log(response)   --> revisamos si la URL en este caso no tiene el prefijo https://cataas.com
+          })
+      })
   }, [])
 
   return (
     <main>
       <h1>App de gatitos</h1>
-      <p>{fact}</p>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`}
+                     />}
+
+      </section>
     </main>
 
   )
