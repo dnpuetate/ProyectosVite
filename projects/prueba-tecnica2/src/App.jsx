982a29ae297cdac51fa2f2c727042623505d69c6
dnpuetate <49051986+dnpuetate@users.noreply.github.com>
2023-09-11 20:36:33 -0500

    PT- CUSTOM HOOK PT1

--- projects/prueba-tecnica2/src/App.jsx
+++ projects/prueba-tecnica2/src/App.jsx
@@ -1,22 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+import { getRandomFact } from './services/facts'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
-export function App () {
-  const [fact, setFact] = useState()
+
+function useCatImage ({ fact }) {
   const [imageUrl, setImageUrl] = useState()
-
-  // Recuperar la cita al cargar la página
-
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
-  }, [])
 
   // Recuperar la imagen cada vez que tenemos una cita nueva
   useEffect(() => {
@@ -32,10 +21,27 @@ export function App () {
       })
   }, [fact])
 
+  return { imageUrl }
+} // {imageUrl: 'https:// ...'}
+
+export function App () {
+  const [fact, setFact] = useState()
+  const { imageUrl } = useCatImage({ fact })
+  // para recuperar la cita al cargar la pagina
+  useEffect(() => {
+    getRandomFact().then(newFact => setFact(newFact))
+  }, [])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
+
   return (
     <main>
       <h1>App de gatitos</h1>
       <section>
+        <button onClick={handleClick}>Get new fact</button>
         {fact && <p>{fact}</p>}
         {imageUrl && <img
           src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`}
